e1c0f5523efdbf710224f6f3096458445128641c
Samuel <samugit.projects@gmail.com>
2024-02-27 13:05:00 -0300

    Aula01 O que é internacionalização

---
+++ src/theory/I18nConcepts.java
@@ -0,0 +1,24 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class I18nConcepts {
+
+    /*
+    * O que é internacionalização
+    *
+    * O professor apresentou uma foto de interface gráfica em dois idiomas do que se assemelha a um editor de textos com os menus /janelas a seguir
+    *
+    *  Text Editor           Editor de Textos
+    *    File  Edit             Arquivo  Editar
+    *       New                     Novo
+    *       Open                    Abrir
+    *       Save                    Salvar
+    *       Save As...              Salvar Como...
+    *       Exit                    Sair
+    *
+    * Quando criamos uma aplicação / sistema apto para atender diversas nacionalidades ou regiões.
+    * O termo Internationalization também é chamado de i18n, sendo i a primeira letra, n a última e 18 a quantidade de letras entre a primeira e a última
+    * Outros elementos além de texto podem seguir a internacionalização como: números, moedas, datas e horas.
+    *
+    * */
+}
